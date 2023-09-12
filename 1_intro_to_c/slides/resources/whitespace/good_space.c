a0ceb9c8f1f96430d439596aafd6579bbf8db058
Andrew Holmes <acholmes001@gmail.com>
2023-09-11 20:47:31 -0400

    tidy up

--- 1_intro_to_c/slides/resources/whitespace/good_space.c
+++ 1_intro_to_c/slides/resources/whitespace/good_space.c
@@ -2,6 +2,6 @@
 
 int main (void) {
     int s1, s2, s3, s4, s5, s6, s7;
-    printf("My sequence: %i, %i, %i, %i, %i, %i, %i...",
+    printf("My sequence: %i, %i, %i, %i, %i, %i, %i", 
                          s1, s2, s3, s4, s5, s6, s7);
 }
